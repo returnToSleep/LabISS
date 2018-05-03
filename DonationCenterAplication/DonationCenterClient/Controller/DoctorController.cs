93a52444bc19ba40b1b42ca75fe9090d769bc988
Abrudean Sergiu <31239261+ASergiuV@users.noreply.github.com>
2018-05-03 21:51:31 +0300

    Added makeRequest, reviewBlood for DoctorController

--- DonationCenterAplication/DonationCenterClient/Controller/DoctorController.cs
+++ DonationCenterAplication/DonationCenterClient/Controller/DoctorController.cs
@@ -4,17 +4,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Server.Remoting;
 
 namespace Controller{
     /**
      * 
      */
+    
     public class DoctorController {
 
         /**
          * 
          */
+
+        private ServerService service;
+
         public DoctorController() {
+            this.service = new ServerService();
         }
 
         /**
@@ -27,9 +33,10 @@ namespace Controller{
          * @param val 
          * @return
          */
-        public void makeRequest(Location val) {
-            // TODO implement here
-            return;
+        public void makeRequest(Location val, int priority, string patientName, string requestString) {
+            //get donation center id from location TODO
+            DoctorRequest req = new DoctorRequest(doctor.id, donationCeter_id, priority, patientName, requestString);
+            this.service.AddToDatabase(req);
         }
 
         /**
@@ -44,30 +51,32 @@ namespace Controller{
          * @param val 
          * @return
          */
-        public void reviewBloodStocks(Location val) {
-            // TODO implement here
-            return;
+        public Tuple<IList<Plasma>, IList<Trombocytes>, IList<RedBloodCells>> reviewBloodStocks(Location val) {
+            var tuple = Tuple.Create(IList < Plasma > this.service.GetAllFromDatabase<Plasma>(),
+                IList < Trombocytes > this.service.GetAllFromDatabase<Trombocytes>(),
+                IList < RedBloodCells > this.service.GetAllFromDatabase<RedBloodCells>());
+            return tuple;
         }
 
         /**
          * @param name
          */
         public void setDoctorName(String name) {
-            // TODO implement here
+            doctor.name = name;
         }
 
         /**
          * @param newVal
          */
-        public void detDoctorSpeciality(String newVal) {
-            // TODO implement here
+        public void setDoctorSpeciality(String newVal) {
+            doctor.speciality = newVal;
         }
 
         /**
          * @param newVal
          */
         public void setHospita(String newVal) {
-            // TODO implement here
+            doctor.hospital = newVal;
         }
 
     }
