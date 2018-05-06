d5bfa2df2818cc92813ba5e2637e912e76365782
Bendic Radu <brie2063@scs.ubbcluj.ro>
2018-05-06 20:03:29 +0300

    Updated database

---
+++ PopulateDatabase.sql
@@ -0,0 +1,46 @@
+INSERT INTO DonationCenter(id, name) VALUES
+('46.789348,23.607177', 'Centru donare sange'),
+('46.774421,23.594765', 'Centrul Crucea Rosie'),
+('46.780379,23.585438', 'Centrul Make-A-Wish'),
+('46.766539,23.62596' , 'Centrul Save-A-Life'),
+('46.751727,23.594825', 'Centrul Never-Too-Late')
+
+INSERT INTO Doctor(name, speciality, latitude, longitude, hospital) VALUES
+('Baican Vlad','Psihoterapeut','46.789348','23.607177', 'Spitalul Municipial Cluj-Napoca'),
+('Eusebiu Macalache','Chirurg','46.789348','23.607177', 'Spitalul Municipial Cluj-Napoca'),
+('Gligor Ispas Traian','Neurochirurg','46.789348','23.607177', 'Spitalul Municipial Cluj-Napoca'),
+('Ciotoroschi Gabriel','Fizioterapeut','46.755894','23.583707','Spitalul de recuperare'),
+('Betean Cristina','Chirurg','46.755894','23.583707','Spitalul de recuperare'),
+('Priscariu Adina Gabriela','Neurochirurg','46.755894','23.583707','Spitalul de recuperare'),
+('Leon Danaila','Neurochirurg','46.760423','23.583998','Spitalul pentru boli infectioase'),
+('Molodoi Emanuel Catalin','Chirurg','46.760423','23.583998','Spitalul pentru boli infectioase'),
+('Scarlat Elena Mihaela','Medic Specialist','46.760423','23.583998','Spitalul pentru boli infectioase'),
+('Irimie Cezar Petrescu','Chirurg','46.760423','23.583998','Spitalul pentru boli infectioase')  
+
+
+
+INSERT INTO DONOR VALUES
+('Costin Vaduva', '1001', '1974-03-21', 'costi@gmail.com', 1, '46.789348,23.607177', 46.7688661484858, 23.6077523231506),
+('Constantin Bucur', '1002', '1980-12-22', 'constant_bucur@vahpp.com', 1, '46.789348,23.607177', 46.7657502339781, 23.6087822914124),
+('Petru Vântu', '1003', '1999-01-07', 'petrutz@vahpp.com', 1, '46.774421,23.594765', 46.7657502339781, 23.6087822914124),
+('Neculai Moisuc', '1004', '1996-09-01', 'moisucmoisuc@vahpp.com', 0, '46.780379,23.585438', 46.7692629737148, 23.5951137542725),
+('Stelian Piturca', '1005', '2000-04-01', 'steaua4life@hmail.com', 0, '46.774421,23.594765', 46.7650741155231, 23.5788059234619),
+('Beniamin Fidatov', '1006', '1993-03-23', 'bannyFF@vahpp.com', 1, '46.751727,23.594825', 46.7614288089536, 23.5763168334961),
+('Mazonn Minea', '1007', '1999-05-02', 'mmoozz@ge.com', 1, '46.751727,23.594825', 46.7689396348599, 23.5697937011719),
+('Razvan Mihaili', '1008', '1988-11-30', 'razvy@vahpp.com', 0, '46.774421,23.594765', 46.7750239588042, 23.586208820343),
+('Ionut Georgescu', '1009', '1991-01-20', 'ionutz@vahpp.com', 1, '46.766539,23.62596', 46.7789328477271, 23.6042976379395),
+('Mihaita Dimir', '1010', '1994-06-24', 'mihaitadimir@vahpp.com', 0, '46.751727,23.594825',46.774818219948, 23.6155843734741),
+('Melania Dita', '2000', '2000-03-22', 'melania_dita@vahpp.com', 0, '46.789348,23.607177', 46.7666174169655, 23.626012802124),
+('Monica Tanase', '2001', '1979-12-12', 'mony@vahpp.com', 1, '46.751727,23.594825', 46.7588857675993, 23.6157560348511),
+('Luiza Stefoniou', '2002', '1988-05-25', 'stefanoiu.luiza@vahpp.com', 0, '46.789348,23.607177', 46.7614435082657, 23.6039972305298),
+('Ioana Grul', '2003', '1993-12-10', 'ioanaG@vahpp.com', 1, '46.766539,23.62596', 46.7825181955654, 23.5891270637512),
+('Regina Tudor', '2004', '1995-01-01', 'royaltyTudor@vahpp.com', 1, '46.789348,23.607177', 46.7529025322143, 23.5508680343628),
+('Daniela Paler', '2005', '1998-02-25', 'paler_dana@vahpp.com', 0, '46.780379,23.585438', 46.7615758018942, 23.5637640953064),
+('Erika Popa', '2006', '1999-01-10', 'erikka@gmailka.com', 1, '46.789348,23.607177', 46.7511971149711, 23.5664033889771),
+('Lizuca Voiculescu', '2007', '1976-07-19', 'dumbravaMinunata@vahpp.com', 0, '46.751727,23.594825', 46.7542550661381, 23.5907793045044),
+('Rozalia Musat', '2008', '1996-01-10', 'musat.rozalia@vahpp.com', 0, '46.789348,23.607177', 46.7603116494996, 23.6077952384949),
+('Adela Iagar', '2009', '1992-12-15', 'adela_iagar@vahpp.com', 0, '46.766539,23.62596', 46.7603116494996, 23.6077952384949)
+
+
+INSERT INTO DONOR VALUES
+('Costin Vaduva', '4009', '1974-03-21', 'costi@gmail.com', 1, '46.789348,23.607177', 46.7688661484858, 23.6077523231506)
