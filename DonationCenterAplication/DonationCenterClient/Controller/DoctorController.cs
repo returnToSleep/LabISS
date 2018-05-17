a894fa354884c924488f73eb7bdef492e58c41a4
Abrudean Sergiu <31239261+ASergiuV@users.noreply.github.com>
2018-05-17 23:08:04 +0300

    Updated DoctorController

--- DonationCenterAplication/DonationCenterClient/Controller/DoctorController.cs
+++ DonationCenterAplication/DonationCenterClient/Controller/DoctorController.cs
@@ -45,95 +45,10 @@ namespace Controller
          * @param val 
          * @return
          */
-        public void makeRequest(Location val, int priority, string patientName, string patientCNP, string requestString)
+        public void makeRequest(Location val, int priority, string patientName, string requestString)
         {
-            bool foundRequest = false;
-            string donationCenterLocation = val.latitude.ToString() + ',' + val.longitude.ToString();
-            DoctorRequest req = new DoctorRequest
-            {
-                donationCenter_id = donationCenterLocation,
-                priority = priority,
-                doctor_name = doctor.name,
-                patientCnp = patientCNP,
-                requestString = requestString,
-                doctor_id = doctor.id,
-                hospital = doctor.hospital
-
-            };
-            List<string> requestSplitted = requestString.Split(',').ToList<string>();
-
-            switch (requestSplitted[0])
-            {
-                case "Red":
-                    {
-                        foreach (var dbRequest in this.service.GetAllFromDatabase<DoctorRequest>())
-                        {
-                            if (dbRequest.donationCenter_id == donationCenterLocation && dbRequest.isBeeingDelivered == false)
-                            {
-                                List<string> dbRequestSplitted = dbRequest.requestString.Split(',').ToList<string>();
-                                if (dbRequestSplitted[0] == requestSplitted[0] && dbRequestSplitted[1] == requestSplitted[1] && dbRequestSplitted[2] == requestSplitted[2])
-                                {
-                                    string newRequestString = dbRequestSplitted[0] + ',' + dbRequestSplitted[1] + ',' + dbRequestSplitted[2] + ',';
-                                    double amount = Convert.ToDouble(dbRequestSplitted[3]);
-                                    amount += Convert.ToDouble(requestSplitted[3]);
-                                    newRequestString += amount.ToString();
-                                    dbRequest.requestString = newRequestString;
-                                    foundRequest = true;
-                                    this.service.UpdateOneFromDatabase(dbRequest);
-                                }
-                            }
-                        }
-                        break;
-                    }
-                case "Plasma":
-                    {
-                        foreach (var dbRequest in this.service.GetAllFromDatabase<DoctorRequest>())
-                        {
-                            if (dbRequest.donationCenter_id == donationCenterLocation && dbRequest.isBeeingDelivered == false)
-                            {
-                                List<string> dbRequestSplitted = dbRequest.requestString.Split(',').ToList<string>();
-                                if (dbRequestSplitted[0] == requestSplitted[0] && dbRequestSplitted[1] == requestSplitted[1])
-                                {
-                                    string newRequestString = dbRequestSplitted[0] + ',' + dbRequestSplitted[1] + ',';
-                                    double amount = Convert.ToDouble(dbRequestSplitted[2]);
-                                    amount += Convert.ToDouble(requestSplitted[2]);
-                                    newRequestString += amount.ToString();
-                                    dbRequest.requestString = newRequestString;
-                                    foundRequest = true;
-                                    this.service.UpdateOneFromDatabase(dbRequest);
-                                }
-                            }
-                        }
-                        break;
-                    }
-                case "Tromb":
-                    {
-
-                        foreach (var dbRequest in this.service.GetAllFromDatabase<DoctorRequest>())
-                        {
-                            if (dbRequest.donationCenter_id == donationCenterLocation && dbRequest.isBeeingDelivered == false)
-                            {
-                                List<string> dbRequestSplitted = dbRequest.requestString.Split(',').ToList<string>();
-                                if (dbRequestSplitted[0] == requestSplitted[0])
-                                {
-                                    string newRequestString = dbRequestSplitted[0] + ',';
-                                    double amount = Convert.ToDouble(dbRequestSplitted[1]);
-                                    amount += Convert.ToDouble(requestSplitted[1]);
-                                    newRequestString += amount.ToString();
-                                    dbRequest.requestString = newRequestString;
-                                    foundRequest = true;
-                                    this.service.UpdateOneFromDatabase(dbRequest);
-                                }
-                            }
-                        }
-                        break;
-                    }
-                default:
-                    break;
-            }
-            if (foundRequest == false)
-                this.service.AddToDatabase(req);
-
+            DoctorRequest req = new DoctorRequest(doctor.id, val.latitude.ToString() + ',' + val.longitude.ToString(), priority, patientName, requestString);
+            this.service.AddToDatabase(req);
         }
 
         /**
@@ -157,6 +72,54 @@ namespace Controller
                 this.service.GetAllFromDatabase<RedBloodCell>());
             
         }
+        public BloodComponent matchBloodWithRequest(DoctorRequest docReq)
+        {
+            string[] reqInfo = docReq.requestString.Split(',');
+            if(reqInfo[0] == "Red")
+            {
+                foreach(var bloodCell in this.doctor.redBloodCellList)
+                {
+                    if(bloodCell.antigen == reqInfo[1] && bloodCell.rh == reqInfo[2]
+                        && bloodCell.ammount == float.Parse(reqInfo[3]))
+                    {
+                        return bloodCell;
+                    }
+                }
+            }
+            else if(reqInfo[0] == "Tromb")
+            {
+                foreach (var trombocyte in this.doctor.trombocyteList)
+                {
+                    if (trombocyte.ammount == float.Parse(reqInfo[1]))
+                    {
+                        return trombocyte;
+                    }
+                }
+            }
+            else
+            {
+                foreach (var plasma in this.doctor.plasmaList)
+                {
+                    if (plasma.antibody == reqInfo[1] && plasma.ammount == float.Parse(reqInfo[2]))
+                    {
+                        return plasma;
+                    }
+                }
+            }
+        }
+        public T acceptBlood<T>(T component)
+        {//TODO functie care sa dea valoare lui bloodisok din gui
+            if (bloodIsOk)
+            {
+                this.service.DeleteFromDatabase(component);
+            }
+            else
+            {
+                component.doctor_id = null;
+                component.isBeeingDeliverd = false;
+            }
+        }
+
 
         /**
          * @param name
