0902aa8fc3826acd7a310f59228e836cab5ad0cf
BaicanVlad <vlad.baican@algotech.solutions>
2018-06-05 19:38:37 +0300

    Add donor tests.

--- DonationCenterAplication/Test/Tests/DonorTest.cs
+++ DonationCenterAplication/Test/Tests/DonorTest.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Controller;
 using Test.TestService;
 using Common.Model;
+using System.Collections.Generic;
 
 namespace Test.Tests
 {
@@ -11,12 +12,18 @@ namespace Test.Tests
     {
 
         public DonorController donorControllerFactory()
-        { 
+        {
             TestRemotingService testService = new TestRemotingService();
 
-            Random rng = new Random();
-            var donorList = testService.GetAllFromDatabase<Donor>();
-            Donor donor = donorList[rng.Next(donorList.Count - 1)];
+            Donor donor = new Donor("1971210016651", "Donor101", new DateTime(1997, 12, 10), "Acasa", new Location("Cluj-Napoca, Calea Manastur nr. 5", 46.762467, 23.570993), "mail");
+
+            try
+            {
+                testService.GetOneFromDatabase<Donor>("1971210016651");
+            }catch (Exception)
+            {
+                testService.AddToDatabase(donor);
+            }
 
             return new DonorController(testService, donor);
         }
@@ -25,13 +32,51 @@ namespace Test.Tests
         [TestMethod]
         public void Test_isDonorFit()
         {
-            //TODO VLAD AJUTA-MA SA TE TREC 
+            DonorController dc = donorControllerFactory();
+
+            //Donor is in the pending list
+            Assert.AreEqual(dc.isDonorFit(), "Medical results pending");
+
+            dc.donor.isPending = false;
+
+            //Donor is fit to donate
+            Assert.AreEqual(dc.isDonorFit(), "Good to go!");
+
+            Donation don = new Donation("test", 10f, 10, 10, new DateTime(2018, 06, 04), 10f, 10f, 10f);
+            IList<Donation> donationList = new List<Donation>();
+            donationList.Add(don);
+            dc.donor.donationHistory=donationList;
+
+            //Donor donated in the last 2 months -> not fit
+            Assert.AreNotEqual("Good to go!", dc.isDonorFit());
+            Assert.AreNotEqual("Medical results pending", dc.isDonorFit());
         }
 
         [TestMethod]
         public void Test_getLastDonation()
         {
-            //TODO VLAD AJUTA-MA SA TE TREC 
+            DonorController dc = donorControllerFactory();
+            //test for null donation
+            Assert.IsNull(dc.getDonationHistory());
+            Assert.AreEqual(dc.donor.getLastDonation(), new DateTime(1,1,1));
+
+            IList<Donation> donationList = new List<Donation>();
+            donationList.Add(new Donation("test", 10f, 10, 10, new DateTime(2018, 06, 04), 10f, 10f, 10f));
+            dc.donor.donationHistory = donationList;
+
+            //testing for the latest donation
+            Assert.AreEqual(dc.donor.getLastDonation(), new DateTime(2018, 06, 04));
+
+            //adding older donations
+            donationList.Add(new Donation("test", 10f, 10, 10, new DateTime(2017, 06, 04), 10f, 10f, 10f));
+            donationList.Add(new Donation("test", 10f, 10, 10, new DateTime(2016, 06, 04), 10f, 10f, 10f));
+            donationList.Add(new Donation("test", 10f, 10, 10, new DateTime(2015, 06, 04), 10f, 10f, 10f));
+
+            dc.donor.donationHistory = donationList;
+
+            //the last donation is the most recent one, hence 2018/06/04
+            Assert.AreEqual(dc.donor.getLastDonation(), new DateTime(2018, 06, 04));
+
         }
     }
 }
