e67e9f0834bc5eac726cbe4a3a0b4b7553b896b1
Abrudean Sergiu <31239261+ASergiuV@users.noreply.github.com>
2018-06-06 17:10:49 +0300

    Added DoctorTests

--- DonationCenterAplication/Test/Tests/DoctorTest.cs
+++ DonationCenterAplication/Test/Tests/DoctorTest.cs
@@ -215,13 +215,160 @@ namespace Test
         [TestMethod]
         public void Test_matchBloodWithRequest()
         {
-            //TODO Biju
+            /*
+             * Returns the component that matches the request based on requestString
+             * 
+             * requestString
+             *      Type,[Group][Rh],Quantity
+             * 
+             * Examples:
+             *      requestString for RedBloodCells 
+             *           "Red,A,true,100" 
+             *           "Red,AB,false,200"
+             *      
+             *      requestString for Plasma 
+             *           "Plasma,B,100" 
+             *           "Plasma,0,200"
+             *       
+             *       requestString for Trombocytes
+             *           "Tromb,100"
+             *           "Tromb,200"
+             */
+            var doctorController = this.doctorControllerFactory();
+            #region FillData
+            var tromb = new Trombocyte
+            {
+                ammount = 100,
+                donatedFor = "TestDonatedFor",
+                donationDate = DateTime.Parse("2018-05-30"),
+                donationCenter_id = "1,1"
+            };
+            var red = new RedBloodCell
+            {
+                ammount = 100,
+                donatedFor = "TestDonatedFor",
+                donationDate = DateTime.Parse("2018-05-30"),
+                donationCenter_id = "1,1",
+                antigen = "B",
+                rh = false
+            };
+            var plasma = new Plasma
+            {
+                ammount = 100,
+                donatedFor = "TestDonatedFor",
+                donationDate = DateTime.Parse("2018-05-30"),
+                donationCenter_id = "1,1",
+                antibody = "B"
+            };
+
+            DonationCenter don = new DonationCenter("1,1", "test");
+
+            doctorController.service.AddToDatabase(don);
+            doctorController.service.AddToDatabase(tromb);
+            doctorController.service.AddToDatabase(red);
+            doctorController.service.AddToDatabase(plasma);
+
+            #endregion
+            //Tromb test
+            var docReq = new DoctorRequest(1,"1,1",1,"testPacientName", "Tromb,100","test");
+            var result = doctorController.matchBloodWithRequest(docReq);
+            Assert.AreEqual(result.amount, 100);
+            Assert.IsTrue(result is Trombocyte);
+
+            //Plasma test
+            var docReq = new DoctorRequest(1, "1,1", 1, "testPacientName", "Plasma,B,100", "test");
+            var result = doctorController.matchBloodWithRequest(docReq);
+            Assert.AreEqual(result.amount, 100);
+            Assert.AreEqual(result.antibody, "B");
+            Assert.IsTrue(result is Plasma);
+
+            //Red test
+            var docReq = new DoctorRequest(1, "1,1", 1, "testPacientName", "Red,AB,false,200", "test");
+            var result = doctorController.matchBloodWithRequest(docReq);
+            Assert.AreEqual(result.amount, 200);
+            Assert.IsTrue(result is RedBloodCell);
+            Assert.IsTrue(!result.rh);
+            Assert.AreEqual(result.antigen, "AB");
+
+            //DELETE NEW DATA
+            doctorController.service.DeleteFromDatabase(tromb);
+            doctorController.service.DeleteFromDatabase(red);
+            doctorController.service.DeleteFromDatabase(plasma);
+            doctorController.service.DeleteFromDatabase(don);
         }
 
         [TestMethod]
         public void Test_acceptBlood()
         {
-            //TODO Biju
+            /*
+             * This one is a bit tricky
+             * 
+             * If the doctor accepts the blood
+             *      then it and the request are removed from the database
+             * else 
+             *      The way blood delivery works:
+             *          donationCenter_Id from the component is set to null 
+             *          doctor_Id is set to request.doctor_Id
+             *          request.isBeeingDelivered is set to true
+             *          
+             *      That way, the component no longer appears in the donation center gui (beeing delivered)
+             *      and can be added back if the doctor refuses it
+             */
+            var doctorController = this.doctorControllerFactory();
+            #region FillData
+            var tromb = new Trombocyte
+            {
+                ammount = 100,
+                donatedFor = "TestDonatedFor",
+                donationDate = DateTime.Parse("2018-05-30"),
+                donationCenter_id = "1,1"
+            };
+            var red = new RedBloodCell
+            {
+                ammount = 100,
+                donatedFor = "TestDonatedFor",
+                donationDate = DateTime.Parse("2018-05-30"),
+                donationCenter_id = "1,1",
+                antigen = "B",
+                rh = false
+            };
+            var plasma = new Plasma
+            {
+                ammount = 100,
+                donatedFor = "TestDonatedFor",
+                donationDate = DateTime.Parse("2018-05-30"),
+                donationCenter_id = "1,1",
+                antibody = "B"
+            };
+
+            DonationCenter don = new DonationCenter("1,1", "test");
+
+            doctorController.service.AddToDatabase(don);
+            doctorController.service.AddToDatabase(tromb);
+            doctorController.service.AddToDatabase(red);
+            doctorController.service.AddToDatabase(plasma);
+
+            #endregion
+            //Tromb test
+            var docReq = new DoctorRequest(1, "1,1", 1, "testPacientName", "Tromb,100", "test");
+            int first = doctorController.service.GetAllFromDatabase<Trombocyte>();
+            doctorController.acceptBlood(docReq,tromb,true);
+            Assert.IsTrue(first > doctorController.service.GetAllFromDatabase<Trombocyte>());
+
+            //Plasma test
+            var docReq = new DoctorRequest(1, "1,1", 1, "testPacientName", "Plasma,B,100", "test");
+            int first = doctorController.service.GetAllFromDatabase<Plasma>();
+            doctorController.acceptBlood(docReq,plasma,true);
+            Assert.IsTrue(first > doctorController.service.GetAllFromDatabase<Plasma>());
+
+            //Red test
+            var docReq = new DoctorRequest(1, "1,1", 1, "testPacientName", "Red,AB,false,200", "test");
+            int first = doctorController.service.GetAllFromDatabase<RedBloodCell>();
+            doctorController.acceptBlood(docReq,red,true);
+            Assert.IsTrue(first > doctorController.service.GetAllFromDatabase<RedBloodCell>());
+
+            //DELETE NEW DATA
+            doctorController.service.DeleteFromDatabase(don);
         }
 
 
